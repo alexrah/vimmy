ec4602c5272e644624321fed0d861735c015dd54
root <root@stk>
2016-02-27 01:51:30 +0100

    shell: init updated

--- shell_scripts/init.sh
+++ shell_scripts/init.sh
@@ -13,4 +13,5 @@ ln -s .vim/.dir_colors.NEW .dir_colors
 ln -s .vim/.tmux.conf
 cd .vim
 git submodule update --init
+sed -i 's/base_color=/base_color=linux:normal=white,black:marked=yellow,black:input=,green:menu=black:menusel=white:menuhot=red,:menuhotsel=black,red:dfocus=white,black:dhotnormal=white,black:dhotfocus=white,black:executable=,black:directory=white,black:link=white,black:device=white,black:special=white,black:core=,black:stalelink=red,black:editnormal=white,black/g' ~/.config/mc/ini
 chsh -s /bin/zsh
