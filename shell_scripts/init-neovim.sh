9131ac9e04ad16a3afc86b2110befe0d9ca9e62b
alessandro stoppato <alexrah@gmail.com>
2020-07-24 10:02:17 +0200

    shell_scripts/init-neovim.sh: work-in-prog install ripgrep, fzf, python2 neovim, python3 neovim, node neovim, ruby neovim, coc intellisense extensions

--- shell_scripts/init-neovim.sh
+++ shell_scripts/init-neovim.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-nvim -c 'CocInstall -sync coc-phpls coc-python'
+
+
+
+# MacOS
+brew install ripgrep
+brew install fzf
+
+# add scripting provider - check what's supported :checkhealth provider
+# install pip @see https://www.gungorbudak.com/blog/2018/08/02/correct-installation-and-configuration-of-pip2-and-pip3/
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo python3 get-pip.py
+pip2 install neovim
+pip3 install neovim
+npm install -g neovim
+gem install neovim
+
+#NeoVim Intellisense with LSP for PHP & Python - @see https://github.com/neoclide/coc.nvim/wiki/Using-coc-extensions 
+nvim -c 'CocInstall -sync coc-phpls coc-python coc-sh coc-tsserver coc-html coc-git coc-emmet coc-css coc-snippets'
 
