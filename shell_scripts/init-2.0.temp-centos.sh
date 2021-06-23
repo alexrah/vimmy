efab65a2b74ddb0a15225b66912508c9f2f41d64
Alessandro Stoppato <alexrah@gmail.com>
2021-06-23 23:29:37 +0200

    shell_scripts/init-2.0.temp-centos.sh: WIP stripped-down for Centos

--- shell_scripts/init-2.0.temp-centos.sh
+++ shell_scripts/init-2.0.temp-centos.sh
@@ -30,14 +30,34 @@ esac
 # $PACKAGE_MANAGER install git
 # $PACKAGE_MANAGER install zsh
 # $PACKAGE_MANAGER install neovim
+printf "install tmux...\n"
 $PACKAGE_MANAGER install tmux
-$PACKAGE_MANAGER install ripgrep
-$PACKAGE_MANAGER install fzf
+
+printf "install ripgrep...\n"
+# $PACKAGE_MANAGER install ripgrep
+yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+sudo yum install ripgrep
+
+printf "install fzf...\n"
+# $PACKAGE_MANAGER install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/fzf
+cd fzf/
+./install
+cd ~
+
+printf "install nodejs...\n"
 $PACKAGE_MANAGER install nodejs
-$PACKAGE_MANAGER install python
-$PACKAGE_MANAGER install python3
-$PACKAGE_MANAGER install ruby
-$PACKAGE_MANAGER install bat
+
+# $PACKAGE_MANAGER install python
+# $PACKAGE_MANAGER install python3
+# $PACKAGE_MANAGER install ruby
+
+# $PACKAGE_MANAGER install bat
+wget -O bat.tar.gz https://github.com/sharkdp/bat/releases/download/v0.7.1/bat-v0.7.1-x86_64-unknown-linux-musl.tar.gz
+tar xvzf bat.tar.gz
+cd bat/
+sudo mv bat /usr/local/bin/bat
+cd ~
 
 
 #ZSH & DOTFILES
@@ -54,7 +74,6 @@ git submodule update --init
 cd ..
 ln -s .vim/.zshrc
 ln -s .vim/.vimrc
-ln -s .vim/.ctags
 ln -s .vim/.dir_colors.NEW .dir_colors
 ln -s .vim/.tmux.conf
 mkdir -p ~/.vim_runtime/undodir
@@ -64,53 +83,13 @@ chsh -s /bin/zsh
 
 
 # NEOVIM 
-printf "NeoVIM configurations: Python, Python3, NodeJS, Ruby, CoC\n"
+printf "NeoVIM configurations: NodeJS, CoC\n"
 printf "================================\n"
-# add scripting provider - check what's supported :checkhealth provider
-# install pip @see https://www.gungorbudak.com/blog/2018/08/02/correct-installation-and-configuration-of-pip2-and-pip3/
-
-# PYTHON SUPPORT
-if ! command -v pip3 &> /dev/null
-then
-	printf "pip3 not found, installing...\n"
-	wget https://bootstrap.pypa.io/get-pip.py
-	sudo python3 get-pip.py
-fi
-
-# sudo python get-pip.py
-# pip2 install virtualenv
-printf "Python: install virtualenv & create .virtualenvs for neovim python & python3 support...\n"
-printf "================================\n"
-pip3 install virtualenv
-
-# PYTHON VIRTUALENV SUPPORT
-mkdir -p ~/.virtualenvs/neovim-python2
-cd ~/.virtualenvs/neovim-python2
-virtualenv py2 -p $(which python)
-source py2/bin/activate
-pip install neovim
-
-mkdir -p ~/.virtualenvs/neovim-python3
-cd ~/.virtualenvs/neovim-python3
-virtualenv py3 -p $(which python3)
-source py3/bin/activate
-pip install neovim
 
 # NODE SUPPORT
 printf "NodeJS: install neovim support (required by CoC)\n"
 printf "================================\n"
 npm install -g neovim
-
-# RUBY SUPPORT
-printf "Ruby: install neovim support\n"
-printf "================================\n"
-if ! command -v gem &> /dev/null
-then
-	printf "gem could not be found\n"
-	exit
-else
-	gem install neovim
-fi
 
 # Symlinks init.vim & coc-settings.json
 printf "symlinks: init.vim & coc-settings.json in "$NVIM_CONFIG_PATH"\n"
