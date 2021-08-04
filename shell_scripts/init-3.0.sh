a3fa18dfda517520279dced413699629093e898c
alessandro stoppato <alexrah@gmail.com>
2021-08-04 14:08:58 +0200

    shell_scripts/init-3.0.sh: full support for Ubuntu & Centos

--- shell_scripts/init-3.0.sh
+++ shell_scripts/init-3.0.sh
@@ -53,19 +53,21 @@ case "$os_type" in
 		printf "OS DETECTED: DEBIAN/UBUNTU\n"
 		export PACKAGE_MANAGER=apt-get
 		export NVIM_CONFIG_PATH=~/.config/nvim
-    curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+    echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo add-apt-repository ppa:x4121/ripgrep
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+    sudo add-apt-repository ppa:git-core/ppa
     sudo apt-get update
     ;;
 	"CentOS Linux")
 		printf "OS DETECTED: CENTOS\n"
 		export PACKAGE_MANAGER=yum
 		export NVIM_CONFIG_PATH=~/.config/nvim
-    curl -sL https://rpm.nodesource.com/setup_14.x | bash -
-    curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
+    curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
     rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
     yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+    curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
     ;;
 	"linux-android")
 		printf "OS DETECTED: TERMUX\n"
@@ -91,7 +93,7 @@ printf "=========> install neovim...\n"
 # sudo $PACKAGE_MANAGER -y install neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod 755 nvim.appimage
-mv nvim.appimage /usr/local/bin/nvim
+sudo mv nvim.appimage /usr/local/bin/nvim
 
 printf "=========> install tmux...\n"
 sudo $PACKAGE_MANAGER -y install tmux
@@ -108,9 +110,7 @@ cd $INSTALLERS_FOLDER/fzf
 cp bin/fzf /usr/local/bin/fzf
 
 printf "=========> install nodejs...\n"
-curl -sL https://rpm.nodesource.com/setup_14.x | bash -
 sudo $PACKAGE_MANAGER -y install nodejs
-
 
 printf "=========> install yarn...\n"
 cd $INSTALLERS_FOLDER
@@ -129,7 +129,7 @@ cd $INSTALLERS_FOLDER
 curl -L https://github.com/sharkdp/bat/releases/download/v0.7.1/bat-v0.7.1-x86_64-unknown-linux-musl.tar.gz -o bat.tar.gz
 tar xvzf bat.tar.gz
 cd bat-v0.7.1-x86_64-unknown-linux-musl
-mv bat /usr/local/bin/bat
+sudo mv bat /usr/local/bin/bat
 
 
 #ZSH & DOTFILES
@@ -137,23 +137,27 @@ printf "clone alexrah/vimmy & alexrah/oh-my-zsh, config zsh, tmux, vim (legacy)\
 printf "================================\n"
 cd $INSTALLERS_FOLDER
 git clone https://alexrah@github.com/alexrah/vimmy 
-cd ~
-git clone https://alexrah@github.com/alexrah/oh-my-zsh .oh-my-zsh
-cd .oh-my-zsh
+git clone https://alexrah@github.com/alexrah/oh-my-zsh oh-my-zsh
+cd oh-my-zsh
 git fetch --all
 git branch --all
 git checkout origin/theme-dstkph
 git submodule update --init
 cd ~
 ln -s $INSTALLERS_FOLDER/vimmy/.zshrc
-ln -s $INSTALLERS_FOLDER/vimmy/.vimrc
+# ln -s $INSTALLERS_FOLDER/vimmy/.vimrc
 ln -s $INSTALLERS_FOLDER/vimmy/.dir_colors.NEW .dir_colors
 ln -s $INSTALLERS_FOLDER/vimmy/.tmux.conf
 ln -s $INSTALLERS_FOLDER/vimmy/.gitconfig
+
+# Symlinks init.vim & coc-settings.json
+printf "symlinks: init.vim & coc-settings.json in "$NVIM_CONFIG_PATH"\n"
+printf "================================\n"
+mkdir -p $NVIM_CONFIG_PATH
+ln -s $INSTALLERS_FOLDER/vimmy/init.vim $NVIM_CONFIG_PATH/init.vim
+ln -s $INSTALLERS_FOLDER/vimmy/coc-settings.json $NVIM_CONFIG_PATH/coc-settings.json
+
 mkdir -p ~/.vim_runtime/undodir
-cd $INSTALLERS_FOLDER/vimmy
-git submodule update --init
-chsh -s /bin/zsh
 cd ~
 
 # NEOVIM 
@@ -162,32 +166,44 @@ printf "================================\n"
 # add scripting provider - check what's supported :checkhealth provider
 # install pip @see https://www.gungorbudak.com/blog/2018/08/02/correct-installation-and-configuration-of-pip2-and-pip3/
 
-# PYTHON SUPPORT
+cd $INSTALLERS_FOLDER
+
+
+printf "Python: install pip & pip3, add neovim python & python3 support...\n"
+printf "================================\n" 
+# PYTHON2 SUPPORT
+if ! command -v pip &> /dev/null
+then
+	printf "pip not found, installing...\n"
+  curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip2.py
+	sudo python get-pip2.py
+fi
+
+# PYTHON3 SUPPORT
 if ! command -v pip3 &> /dev/null
 then
 	printf "pip3 not found, installing...\n"
-	wget https://bootstrap.pypa.io/get-pip.py
-	sudo python3 get-pip.py
+  curl https://bootstrap.pypa.io/get-pip.py -o get-pip3.py
+	sudo python3 get-pip3.py
 fi
+pip install neovim
+pip3 install neovim
 
-# sudo python get-pip.py
-# pip2 install virtualenv
-printf "Python: install virtualenv & create .virtualenvs for neovim python & python3 support...\n"
+printf "Python: install virtualenv & create .virtualenvs...\n"
 printf "================================\n"
-pip3 install virtualenv
-
 # PYTHON VIRTUALENV SUPPORT
-mkdir -p ~/.virtualenvs/neovm-python2
-cd ~/.virtualenvs/neovim-python2
-virtualenv py2 -p $(which python)
-source py2/bin/activate
-pip install neovim
-
-mkdir -p ~/.virtualenvs/neovim-python3
-cd ~/.virtualenvs/neovim-python3
-virtualenv py3 -p $(which python3)
-source py3/bin/activate
-pip install neovim
+# pip3 install virtualenv
+# mkdir -p ~/.virtualenvs/neovm-python2
+# cd ~/.virtualenvs/neovim-python2
+# virtualenv py2 -p $(which python)
+# source py2/bin/activate
+# pip install neovim
+#
+# mkdir -p ~/.virtualenvs/neovim-python3
+# cd ~/.virtualenvs/neovim-python3
+# virtualenv py3 -p $(which python3)
+# source py3/bin/activate
+# pip install neovim
 
 # NODE SUPPORT
 printf "NodeJS: install neovim support (required by CoC)\n"
@@ -205,13 +221,6 @@ else
 	gem install neovim
 fi
 
-# Symlinks init.vim & coc-settings.json
-printf "symlinks: init.vim & coc-settings.json in "$NVIM_CONFIG_PATH"\n"
-printf "================================\n"
-mkdir -p $NVIM_CONFIG_PATH
-ln -s $INSTALLERS_FOLDER/vimmy/init.vim $NVIM_CONFIG_PATH/init.vim
-ln -s $INSTALLERS_FOLDER/vimmy/coc-settings.json $NVIM_CONFIG_PATH/coc-settings.json
-
 # install vim-plug @see https://github.com/junegunn/vim-plug
 printf "Installing NeoVim plugin manager: junegunn/vim-plug...\n"
 printf "================================\n"
@@ -219,5 +228,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 printf "NOTE: run :PlugInstall first time launching nvim\n"
-printf "DONE!\n"
+
+chsh -s /bin/zsh
 zsh
+printf "DONE!\n"
