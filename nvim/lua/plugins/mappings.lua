1eceec073e8fbeafbf5f217ca8cd32eaea908af0
alessandro stoppato <alexrah@gmail.com>
2025-07-24 03:21:53 +0200

    nvim/lua/plugins/mappings.lua: add mapping to Grep current open buffers

--- nvim/lua/plugins/mappings.lua
+++ nvim/lua/plugins/mappings.lua
@@ -9,6 +9,7 @@ return {
           [";"] = { ":" },
           ["<C-v>"] = { "pi", desc = "Paste using standard shortcut" },
           ["<Leader>bm"] = { ":Maximize<Enter>", desc = "Toggle maximizing the current window" },
+          ["<Leader>fB"] = { function() require("snacks").picker.grep_buffers() end, desc = "Grep Open Buffers" },
         },
         v = {
           ["<C-c>"] = { '"+y<Esc>i', desc = "Copy using standard shortcut" },
