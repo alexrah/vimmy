b054a5bb45d8ab24b9cea56354b163f74443baea
alessandro stoppato <alexrah@gmail.com>
2025-07-24 17:24:21 +0200

    nvim/lua/plugins/neo-tree.lua: show hidden files

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
+}
