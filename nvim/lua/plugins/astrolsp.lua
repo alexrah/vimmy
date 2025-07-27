8e3f3eb78131171d50e73cf2afc1a7785bf86a59
alessandro stoppato <alexrah@gmail.com>
2025-07-27 21:18:54 +0200

    lua/plugins/astrolsp.lua: add redundant mappings for LSP definition, declaration & references

--- nvim/lua/plugins/astrolsp.lua
+++ nvim/lua/plugins/astrolsp.lua
@@ -17,5 +17,15 @@ return {
         enabled = false, -- enable or disable format on save globally
       },
     },
+    mappings = {
+      n = {
+        ["<leader>lb"] = { function() vim.lsp.buf.definition() end, desc = "Go to definition" },
+        ["<Leader>lB"] = { function() vim.lsp.buf.declaration() end, desc = "Go to declaration" },
+        ["grd"] = { function() vim.lsp.buf.definition() end, desc = "vim.lsp.buf.definition()" },
+        ["grD"] = { function() vim.lsp.buf.declaration() end, desc = "vim.lsp.buf.declaration()" },
+        ["gd"] = { function() vim.lsp.buf.definition() end, desc = "vim.lsp.buf.declaration()" },
+        ["gb"] = { function() vim.lsp.buf.references() end, desc = "vim.lsp.buf.references()" },
+      },
+    },
   },
 }
