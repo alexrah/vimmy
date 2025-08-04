4706f748d5c0b69114a3112dbc7e9f5d80ec80aa
alessandro stoppato <alexrah@gmail.com>
2025-08-04 16:44:57 +0200

    nvim/lua/plugins/ts-comments.lua: add support for multiple commenting syntaxes

---
+++ nvim/lua/plugins/ts-comments.lua
@@ -0,0 +1,22 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  "folke/ts-comments.nvim",
+  opts = {
+    lang = {
+      javascript = {
+        "/* %s */",
+        "// %s", -- default commentstring when no treesitter node matches
+        call_expression = "// %s", -- specific commentstring for call_expression
+        jsx_attribute = "// %s",
+        jsx_element = "{/* %s */}",
+        jsx_fragment = "{/* %s */}",
+        spread_element = "// %s",
+        statement_block = "// %s",
+      },
+      typescript = { "/* %s */", "// %s" }, -- langs can have multiple commentstrings
+    },
+  },
+  event = "VeryLazy",
+  -- enabled = vim.fn.has "nvim-0.10.0" == 1,
+}
