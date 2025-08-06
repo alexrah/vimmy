3b39b7f103dd9674fe83060af2fb7e940efb7a77
alessandro stoppato <alexrah@gmail.com>
2025-08-06 13:10:13 +0200

    nvim/lua/polish.lua: override WinSeparator to set split border color

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,5 +1,19 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+
+local WinSeparatorFg = "#5A616B"
+
+-- set colors for split separators
+vim.api.nvim_set_hl(0, 'WinSeparator', { fg =  WinSeparatorFg })
+
+require("catppuccin").setup {
+    custom_highlights = function(colors)
+        return {
+            WinSeparator = { fg = WinSeparatorFg },
+        }
+    end
+}
