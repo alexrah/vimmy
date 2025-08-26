c99e11aacdc770d6bc6cf19ad6c890a55fe70d32
alessandro stoppato <alexrah@gmail.com>
2025-08-26 22:28:54 +0200

    nvim/lua/polish.lua: add funtion set_ft_option() to easly add filetype specific options set 4 spaces tabstop for PHP filetype as per PHPCS

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -4,7 +4,7 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
-
+-- INFO: add a border to clearly see the split separators
 local WinSeparatorFg = "#5A616B"
 
 local function set_win_separator()
@@ -19,3 +19,21 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 
 -- Set color on startup
 set_win_separator()
+
+-- INFO: set 4 spaces indent for php as per PHP Coding Standards
+--
+-- https://github.com/neovim/neovim/issues/22768#issuecomment-1482750463
+local function set_ft_option(ft, option, value)
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = ft,
+    group = vim.api.nvim_create_augroup('FtOptions', {}),
+    desc = ('set option "%s" to "%s" for this filetype'):format(option, value),
+    callback = function()
+      vim.opt_local[option] = value
+    end
+  })
+end
+
+
+set_ft_option({'php'}, 'shiftwidth', 4)
+set_ft_option({'php'}, 'tabstop', 4)
