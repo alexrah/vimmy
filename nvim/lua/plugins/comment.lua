d3824c71a2404cc2c06dfafe1e103c0bd5cc3dd7
alessandro stoppato <alexrah@gmail.com>
2025-09-01 12:25:13 +0200

    lua/plugins/comment.lua: enable numToStr/Comment.nvim

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,9 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  "numToStr/Comment.nvim",
+  enabled = true,
+  opts = {
+    -- add any options here
+  },
+}
