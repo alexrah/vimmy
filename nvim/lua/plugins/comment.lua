ca655154e18d89e84e4a622cd9c1f295eddd6259
alessandro stoppato <alexrah@gmail.com>
2025-09-02 02:39:18 +0200

    nvim/lua/plugins/ts-comments.lua: restore ts-comment which has better support for TSX remove comment.lua: conflicts with ts-comments.lua

--- nvim/lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
-return {
-  "numToStr/Comment.nvim",
-  enabled = true,
-  opts = {
-    -- add any options here
-  },
-}
