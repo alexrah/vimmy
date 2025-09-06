48f2d03f26ed36dd05f9dda5a71f0d04ecd24417
Alessandro Stoppato <alexrah@gmail.com>
2025-09-07 00:07:07 +0200

    nvim/lua/community.lua: add Python support

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -5,9 +5,12 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   opts = function(_, opts)
     -- opts variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -17,7 +20,7 @@ return {
     -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
+      -- null_ls.builtins.formatting.ruff_format,
       -- null_ls.builtins.formatting.prettier,
     })
   end,
