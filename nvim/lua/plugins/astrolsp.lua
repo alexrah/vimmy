c0a52b2079d9b0a0f85efba9393c0f44cb3f15da
alessandro stoppato <alexrah@gmail.com>
2025-10-14 15:02:35 +0200

    nvim/lua/plugins/astrolsp.lua: config yamlls to use schemas defined by kubernetes.nvim for k8s

--- nvim/lua/plugins/astrolsp.lua
+++ nvim/lua/plugins/astrolsp.lua
@@ -8,87 +8,104 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
-  ---@type AstroLSPOpts
-  opts = {
-    -- customize lsp formatting options
-    formatting = {
-      -- control auto formatting on save
-      format_on_save = {
-        enabled = false, -- enable or disable format on save globally
+  dependencies = {
+    "diogo464/kubernetes.nvim",
+  },
+  opts = function()
+    ---@type AstroLSPOpts
+    return {
+      -- customize lsp formatting options
+      formatting = {
+        -- control auto formatting on save
+        format_on_save = {
+          enabled = false, -- enable or disable format on save globally
+        },
+        timeout_ms = 2000, -- default format timeout
       },
-      timeout_ms = 2000, -- default format timeout
-    },
-    -- customize language server configuration options passed to `lspconfig`
-    ---@diagnostic disable: missing-fields
-    config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
-      tailwindcss = {
-        settings = {
-          tailwindCSS = {
-            classAttributes = { "class", "className", "ngClass", "class:list", "containerClassName" },
-            classFunctions = { "cn", "clsx", "twMerge" },
+      -- customize language server configuration options passed to `lspconfig`
+      ---@diagnostic disable: missing-fields
+      config = {
+        -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+        tailwindcss = {
+          settings = {
+            tailwindCSS = {
+              classAttributes = { "class", "className", "ngClass", "class:list", "containerClassName" },
+              classFunctions = { "cn", "clsx", "twMerge" },
+            },
           },
         },
-      },
-      html = {
-        filetypes = { "html", "php" },
-      },
-      emmet_ls = {
-        filetypes = {
-          "css",
-          "html",
-          "javascriptreact",
-          "scss",
-          "typescriptreact",
-          "php",
+        html = {
+          filetypes = { "html", "php" },
         },
-      },
-      cssls = {
-        filetypes = { "css", "scss", "less", "html" },
-      },
-      vtsls = {
-        settings = {
-          vtsls = {
-            autoUseWorkspaceTsdk = true,
+        emmet_ls = {
+          filetypes = {
+            "css",
+            "html",
+            "javascriptreact",
+            "scss",
+            "typescriptreact",
+            "php",
           },
         },
-      },
-      intelephense = {
-        settings = {
-          intelephense = {
-            diagnostics = {
-              undefinedMethods = true,
-              undefinedProperties = true,
+        cssls = {
+          filetypes = { "css", "scss", "less", "html" },
+        },
+        vtsls = {
+          settings = {
+            vtsls = {
+              autoUseWorkspaceTsdk = true,
+            },
+          },
+        },
+        intelephense = {
+          settings = {
+            intelephense = {
+              diagnostics = {
+                undefinedMethods = true,
+                undefinedProperties = true,
+              },
+            },
+          },
+        },
+        yamlls = {
+          settings = {
+            yaml = {
+              schemas = {
+                -- use this if you want to match all '*.yaml' files
+                [require("kubernetes").yamlls_schema()] = "*.yaml",
+                -- or this to only match '*.<resource>.yaml' files. ex: 'app.deployment.yaml', 'app.argocd.yaml', ...
+                -- [require("kubernetes").yamlls_schema()] = require("kubernetes").yamlls_filetypes(),
+              },
             },
           },
         },
       },
-    },
-    mappings = {
-      n = {
-        ["<leader>lb"] = { function() vim.lsp.buf.definition() end, desc = "Go to definition" },
-        ["<Leader>lB"] = { function() vim.lsp.buf.declaration() end, desc = "Go to declaration" },
-        ["grd"] = { function() vim.lsp.buf.definition() end, desc = "vim.lsp.buf.definition()" },
-        ["grD"] = { function() vim.lsp.buf.declaration() end, desc = "vim.lsp.buf.declaration()" },
-        ["gd"] = { function() vim.lsp.buf.definition() end, desc = "vim.lsp.buf.declaration()" },
-        ["gb"] = { function() vim.lsp.buf.references() end, desc = "vim.lsp.buf.references()" },
-        ["<C-b>"] = {
-          function() require("snacks.picker").lsp_references { auto_confirm = false } end,
-          desc = "vim.lsp.buf.references()",
-        },
-        ["<C-s>"] = {
-          function() require("snacks.picker").lsp_definitions { auto_confirm = false } end,
-          desc = "vim.lsp.buf.definition()",
-        },
-        ["<C-y>"] = {
-          function() require("snacks.picker").lsp_type_definitions { auto_confirm = false } end,
-          desc = "vim.lsp.buf.type_definition()",
-        },
-        ["<Leader>fy"] = {
-          function() require("snacks.picker").lsp_workspace_symbols() end,
-          desc = "Workspace Symbols",
+      mappings = {
+        n = {
+          ["<leader>lb"] = { function() vim.lsp.buf.definition() end, desc = "Go to definition" },
+          ["<Leader>lB"] = { function() vim.lsp.buf.declaration() end, desc = "Go to declaration" },
+          ["grd"] = { function() vim.lsp.buf.definition() end, desc = "vim.lsp.buf.definition()" },
+          ["grD"] = { function() vim.lsp.buf.declaration() end, desc = "vim.lsp.buf.declaration()" },
+          ["gd"] = { function() vim.lsp.buf.definition() end, desc = "vim.lsp.buf.declaration()" },
+          ["gb"] = { function() vim.lsp.buf.references() end, desc = "vim.lsp.buf.references()" },
+          ["<C-b>"] = {
+            function() require("snacks.picker").lsp_references { auto_confirm = false } end,
+            desc = "vim.lsp.buf.references()",
+          },
+          ["<C-s>"] = {
+            function() require("snacks.picker").lsp_definitions { auto_confirm = false } end,
+            desc = "vim.lsp.buf.definition()",
+          },
+          ["<C-y>"] = {
+            function() require("snacks.picker").lsp_type_definitions { auto_confirm = false } end,
+            desc = "vim.lsp.buf.type_definition()",
+          },
+          ["<Leader>fy"] = {
+            function() require("snacks.picker").lsp_workspace_symbols() end,
+            desc = "Workspace Symbols",
+          },
         },
       },
-    },
-  },
+    }
+  end,
 }
