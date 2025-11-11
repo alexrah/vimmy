2c75c67db56ac265dba6e2f53155b7c38cf73531
alessandro stoppato <alexrah@gmail.com>
2025-11-11 01:13:12 +0100

    nvim/lua/plugins/windsurf.lua: disable within snacks_picker_input

--- nvim/lua/plugins/windsurf.lua
+++ nvim/lua/plugins/windsurf.lua
@@ -17,7 +17,9 @@ return {
       -- Set to true if you never want completions to be shown automatically.
       manual = false,
       -- A mapping of filetype to true or false, to enable virtual text.
-      filetypes = {},
+      filetypes = {
+        ["snacks_picker_input"] = false
+      },
       -- Whether to enable virtual text of not for filetypes not specifically listed above.
       default_filetype_enabled = true,
       -- How long to wait (in ms) before requesting completions after typing stops.
