23ddd31b375743e9584f0943edce43a941e6da09
alessandro stoppato <alexrah@gmail.com>
2026-02-12 12:58:02 +0100

    nvim(conf): plugins/image.lua enable conditionally only in tui envs

--- nvim/lua/plugins/image.lua
+++ nvim/lua/plugins/image.lua
@@ -1,8 +1,11 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
+local is_tui = vim.fn.has "gui_running" == 0
+
 return {
   "3rd/image.nvim",
-  version =  "1.4.0",
+  enabled = is_tui,
+  version = "1.4.0",
   build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
   opts = {
     processor = "magick_cli",
