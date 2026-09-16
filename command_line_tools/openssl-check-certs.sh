1c5e7693d080a22a9bf149cca54509ac2d3ecb33
Alessandro Stoppato <alexrah@gmail.com>
2026-09-16 12:54:13 +0200

    feat(cli):command_line_tools/openssl-check-certs.sh addition checks for certificate and private key matching

--- command_line_tools/openssl-check-certs.sh
+++ command_line_tools/openssl-check-certs.sh
@@ -2,7 +2,17 @@
 
 if [ -n "$1" ]
 then
-  openssl crl2pkcs7 -nocrl -certfile $1 | openssl pkcs7 -print_certs -text -noout
+
+  printf "Public public & private key hashes matching...\n"
+  if ! diff -q <(openssl x509 -noout -pubkey -in $1 | openssl md5) <(openssl pkey -in $1 -pubout | openssl md5)
+  then
+    echo "ERROR: certificate and private key do not match in $1" >&2
+    exit 1
+  fi
+
+  printf "Checking full chain...\n"
+  openssl crl2pkcs7 -nocrl -certfile $1 | openssl pkcs7 -print_certs -text -noout | bat
 else
   echo "Please provide path to *.pem file"
+
 fi
